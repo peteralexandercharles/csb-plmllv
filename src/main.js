7899351d4d10484724727073d5b994b5df1620cd
Code Sandbox <hello@codesandbox.io>
2022-10-21 00:45:09 +0000

    Deployment from Codesandbox.

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/oC9Uo8crw0mqipYg/scene.splinecode');
